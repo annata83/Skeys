98e2f89a98c7007def47fd6bc38e3fd26f69f55d
niko <nicolas.villafan@gmail.com>
2019-07-24 05:07:35 -0300

    webpack initial setup

---
+++ webpack.prod.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // syntax is diferent for this pluging to work
+
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "[name].[contentHash].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimizer: [
+      new HtmlWebpackPlugin({
+        template: "./src/html/index.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+		new CleanWebpackPlugin({
+		// Simulate the removal of files default: false
+    dry: false,
+    // Write Logs to Console// (Always enabled when dry is true) default: false
+    verbose: false,
+    // Automatically remove all unused webpack assets on rebuild default: true
+    cleanStaleWebpackAssets: false,
+ 		// Do not allow removal of current webpack assets default: true
+    protectWebpackAssets: false,
+		})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      }
+    ]
+  }
+});
