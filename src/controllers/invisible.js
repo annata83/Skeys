e69f3229322bc1df384c2c222770fc516106af8c
niko <nicolas.villafan@gmail.com>
2019-09-30 03:36:26 -0300

    working on little things

--- src/controllers/invisible.js
+++ src/controllers/invisible.js
@@ -1,4 +1,21 @@
-const test = function() {
-	console.log("hello fom invisible");
-}
-export {test}
+const jaja = function() {
+	console.log("jaja I got ya!");
+};
+
+const setDisplayEvent = function() {
+	const i3Section = document.getElementById("i3");
+	const rangerSection = document.getElementById("ranger");
+	i3Section.addEventListener( 'click', makeVisible(), false);
+	rangerSection.addEventListener( 'click', makeVisible(), false);
+
+	console.log("Event Set");
+};
+
+const makeVisible = function() {
+	console.log( " I am inside makeVisible " );
+};
+setDisplayEvent();
+makeVisible();
+jaja();
+
+export {setDisplayEvent, makeVisible, jaja}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/styles.scss';
-import {test} from './controllers/invisible';
+import {setDisplayEvent, makeVisibleakeVisible,jaja} from './controllers/invisible';
 
-test();
+
 
 
 
